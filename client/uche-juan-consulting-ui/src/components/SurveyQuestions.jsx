7ec4f86966ba522451529e068d7cfbe5bc1313dc
Daviz8 <okorowhyme234@gmail.com>
2025-02-01 00:41:59 +0100

    finally

--- client/uche-juan-consulting-ui/src/components/SurveyQuestions.jsx
+++ client/uche-juan-consulting-ui/src/components/SurveyQuestions.jsx
@@ -1,34 +1,16 @@
 
-import Question from './Questions';
-
-
 
 function SurveyQuestions() {
-
-let x = 0;
-const Ques = Question[x].question[x];
-
-function Next() {
-
-}
-
-
-function display () {
-const Ans = [];
-  const cat =  Ques.category;
- Ans.push(cat);
-
-}
 
    return(
     <>
 
 <div className='py-36 flex flex-col justify-center items-center'>
 	<div className='bg-white rounded-md pb-2 shadow-lg'>
-		<h1  className='bg-blue-700 px-4 py-3 text-white font-bold rounded-t-md'> {Ques}  </h1>
+		<h1  className='bg-blue-700 px-4 py-3 text-white font-bold rounded-t-md'>  </h1>
 		<div className='flex flex-col w-full gap-3 pt-3 pb-2 px-2 relative'>
 			<div className='relative w-full h-8'>
-				<input type='checkbox' onClick={display}  id='Yes'   className='appearance-none rounded-lg bg-gray-100 cursor-pointer h-full w-full 
+				<input type='checkbox'  id='Yes'   className='appearance-none rounded-lg bg-gray-100 cursor-pointer h-full w-full 
                     checked:bg-blue-400 transition-all duration-200   checked:hover:bg-black-400 hover:bg-blue-800 peer'></input>
 				<label htmlFor='tennis' className='  hover:text-white font-bold absolute top-[50%] left-3 text-gray-400   -translate-y-[50%]
                      peer-checked:text-gray-100 transition-all duration-200 select-none
@@ -60,7 +42,7 @@ const Ans = [];
     </button>
     <button   type="button"  className="bg-black text-white rounded-r-md py-2 border-l border-gray-200 hover:bg-blue-700 hover:text-white px-3">
       <div className="flex flex-row align-middle">
-        <a href="/survey/Next" onClick={Next} className="mr-2">Next</a>
+        <a href="/survey/Next" className="mr-2">Next</a>
         <svg className="w-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
         </svg>
