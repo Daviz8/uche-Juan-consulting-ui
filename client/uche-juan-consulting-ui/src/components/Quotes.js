a0754d4482eaebb672378d456addb39221723aa1
Daviz8 <okorowhyme234@gmail.com>
2025-08-30 10:35:58 +0100

    fixed spelling errors in the quotes

--- client/uche-juan-consulting-ui/src/components/Quotes.js
+++ client/uche-juan-consulting-ui/src/components/Quotes.js
@@ -1,298 +1,289 @@
 const quotes = [    
-
   {
- 
- "id" : 1,
-    "quotes": " Relationships are like live wires they either connect you or Electrocute you ",
-"category" : "Relationships and Marriage"},
-
-{
- "id" : 2,
-"quotes": " Time is Life !",
-"category" : "Life"
-
-},
-
-{
- "id" : 3,
-"quotes":"Never Joke with your crisis because they are there to reposition you " ,
-"category" : "Life"
-},
-
-{
-"id" : 4,
-"quotes": "Every man you meet is a door and every door leads to something.",
-"category":"Life"
-},
-
-{
-"id" : 5,
-"quotes": "Emotions are good followers but bad leaders",
-"category":"Relationships and Marriage"
-},
-{
-"id" : 6,
-"quotes": "Care is Revolutionary",
-"category":"Love"
-},
-{
-"id" : 7,
-"quotes": "your spirit is a channel of information and your mind , a channel for transformation.",
-"category":"Supernatural"
-},
-{
-"id" :8, 
-"quotes":"Respet is positional, The way you position yourself is the way you will be respected " ,
-"category":"Life"
-},
-{
-"id" :9, 
-"quotes":"It is your confession and not your complaint that exempts youu from the rest" ,
-"category":"Supernatural"
-},
-{
-"id" :10, 
-"quotes":"Awareness is the highest level of Light" ,
-"category":"Life"
-},
-{
-"id" :11, 
-"quotes":"starve your distraction, so it does'nt cause you losses" ,
-"category":"Supernatural"
-},
-{
-"id" :12, 
-"quotes":"A growing man is a man with a growing mindset with the capacity for change" ,
-"category":"Life"
-},
-{
-"id" :13, 
-"quotes":"I am Light , I am Salt , I give Taste to My World" ,
-"category":"Supernatural"
-},
-{
-"id" :14, 
-"quotes":"The Capacity of your faith determines the capacity of manifestation that will be delivered to you" ,
-"category":"Supernatural"
-},
-{
-"id" :15 , 
-"quotes":"Man wasn't created for isolation, man was created for connection" ,
-"category":"Life"
-},
-{
-"id" :16, 
-"quotes":"Procastinators are illeterates in destiny , because they cannot discern the signals of opportunities" ,
-"category":"Life"
-},
-{
-"id" :17, 
-"quotes":"God can say a thing but you are the one to create the possibility of that thing" ,
-"category":"Supernatural"
-},
-{
-"id" :18, 
-"quotes":"your heart is very pivotal. When your heart start changing, everything about you changes " ,
-"category":"Supernatural"
-},
-{
-"id" :19, 
-"quotes":"If you take away instructions, it will lead to destruction" ,
-"category":"Life"
-},
-{
-"id" :20, 
-"quotes":"You are not successful unti you can replicate it on others. True prosperity spreads" ,
-"category":"Life"
-},
-{
-"id" :21, 
-"quotes":"Life flows in patterns and every pattern is sustained by principles" ,
-"category":"Life"
-},
-{
-"id" :22, 
-"quotes":"Understanding vision helps you with self leadership" ,
-"category":"Life"
-},
-{
-"id" :23, 
-"quotes":"Following through your process is not about you but who you'll become." ,
-"category":"Life"
-},
-{
-"id" :24, 
-"quotes":" Everybody can do business but not everyone can build a business." ,
-"category":"Life"
-},
-{
-"id" :25, 
-"quotes":"What you costantly take for granted will cause you to loose opportunities" ,
-"category":"Life"
-},
-{
-"id" :26, 
-"quotes":"Your mind is like the soil, whatever you sow into it is what you will get out of it" ,
-"category":"Supernatural"
-},
-{
-"id" :27, 
-"quotes":"Power is the dynamic ablity to cause changes" ,
-"category":"Life"
-},
-{
-"id" :28, 
-"quotes":"The unseen world plays a role in our mindset." ,
-"category":"Supernatural"
-},
-{
-"id" :29, 
-"quotes":"Evil is a product of famine. When there is famine , evil multiplies." ,
-"category":"Life"
-},
-{
-"id" :30, 
-"quotes":"The Life of joy and peace is the beauty of an abundant life" ,
-"category":"Life"
-},
-{
-"id" :31, 
-"quotes":"What you determines what you have. If you can't see it you can't get it." ,
-"category":"Life"
-},
-{
-"id" :32, 
-"quotes":"Consistency conveys your worth, It shows your commitment and builds people's faith in you" ,
-"category":"Life"
-},
-{
-"id" :33, 
-"quotes":"Sometimes speaking is healing and silence is death" ,
-"category":"Life"
-},
-{
-"id" :34, 
-"quotes":"In the journey of Life you need the escorts of wisdom and favour." ,
-"category":"Life"
-},
-{
-"id" :35, 
-"quotes":"We give because we are blessed" ,
-"category":"Life"
-},
-{
-"id" :36, 
-"quotes":"As you rise, have a track record of your rising. it makes Kings look for you." ,
-"category":"Life"
-},
-{
-"id" :37, 
-"quotes":"Don't mistake a canopy for a building" ,
-"category":"Relationships and Marriage"
-},
-{
-"id" :38, 
-"quotes":"The cheapest way to self discovery is Service",
-"category":"Life"
-},
-{
-"id" :39, 
-"quotes":"A man who ends up becoming self made will be self destructive",
-"category":"Life"
-},
-{
-"id" :40, 
-"quotes":"When a man does not have a figure he repects , he will be disfigured in Life",
-"category":"Life"
-},
-{
-"id" :41, 
-"quotes":" Your choice determmines your path. One right choice can set you on the right path.",
-"category":"Life"
-},
-{
-"id" :42, 
-"quotes":"Men show their Works , women display their worth.",
-"category":"Life"
-},
-{
-"id" :43, 
-"quotes":"The toughness of a man is in his mental abilities",
-"category":"Life"
-},
-{
-"id" :44, 
-"quotes":"Men show their work , women show their worth",
-"category":"Life"
-},
-{
-"id" :45, 
-"quotes":"Mercy changes people because it changes their hearts first",
-"category":"Life"
-},
-{
-"id" :46, 
-"quotes":"The reflection of excellence is in your pursuit for knowledge.",
-"category":"Supernatural"
-},
-{
-"id" :47, 
-"quotes":"Whatever eats into your time , eats into your life",
-"category":"Life"
-},
-{
-"id" :48, 
-"quotes":"Excellent people are thinkers and negotiators",
-"category":"Life"
-},
-{
-"id" :49, 
-"quotes":"Attraction is intrested , Reality is committed",
-"category":"Relationships and Marriage"
-},
-{
-"id" :50, 
-"quotes":" your word increases your conciousness and your conciousness powers your reality",
-"category":"Life"
-},
-{
-"id" :51, 
-"quotes":"Being real with yourself is where reality starts",
-"category":"Life"
-},
-{
-"id" :52, 
-"quotes":"Your attraction can be your greatest temptation",
-"category":"Relationships and Marriage"
-},
-{
-"id" :53, 
-"quotes":"Reality is distinctive but attraction is manipulative",
-"category":"Relationships and Marriage"
-},
-{
-"id" :54, 
-"quotes":"As a woman, your first duty to a man is to fuel his courage",
-"category":"Relationships and Marriage"
-},
-{
-"id" :55, 
-"quotes":"Attraction is hasty but reality is patient",
-"category":"Relationships and Marriage"
-},
-{
-"id" :56, 
-"quotes":"Inherent in every woman is a desire to give love and to be loved",
-"category":"Relationships and Marriage"
-},
-{
-"id" :57, 
-"quotes":"Rejection is a chip in your mind that speaks to you negatively",
-"category":"Life"
-}
+    "id": 1,
+    "quotes": "Relationships are like live wires; they either connect you or electrocute you.",
+    "category": "Relationships and Marriage"
+  },
+  {
+    "id": 2,
+    "quotes": "Time is life!",
+    "category": "Life"
+  },
+  {
+    "id": 3,
+    "quotes": "Never joke with your crisis, because they are there to reposition you.",
+    "category": "Life"
+  },
+  {
+    "id": 4,
+    "quotes": "Every man you meet is a door, and every door leads to something.",
+    "category": "Life"
+  },
+  {
+    "id": 5,
+    "quotes": "Emotions are good followers but bad leaders.",
+    "category": "Relationships and Marriage"
+  },
+  {
+    "id": 6,
+    "quotes": "Care is revolutionary.",
+    "category": "Love"
+  },
+  {
+    "id": 7,
+    "quotes": "Your spirit is a channel of information, and your mind a channel for transformation.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 8, 
+    "quotes": "Respect is positional; the way you position yourself is the way you will be respected.",
+    "category": "Life"
+  },
+  {
+    "id": 9, 
+    "quotes": "It is your confession and not your complaint that exempts you from the rest.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 10, 
+    "quotes": "Awareness is the highest level of light.",
+    "category": "Life"
+  },
+  {
+    "id": 11, 
+    "quotes": "Starve your distractions, so they do not cause you losses.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 12, 
+    "quotes": "A growing man is a man with a growing mindset and the capacity for change.",
+    "category": "Life"
+  },
+  {
+    "id": 13, 
+    "quotes": "I am light, I am salt, I give taste to my world.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 14, 
+    "quotes": "The capacity of your faith determines the capacity of manifestation that will be delivered to you.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 15, 
+    "quotes": "Man was not created for isolation; man was created for connection.",
+    "category": "Life"
+  },
+  {
+    "id": 16, 
+    "quotes": "Procrastinators are illiterate in destiny, because they cannot discern the signals of opportunities.",
+    "category": "Life"
+  },
+  {
+    "id": 17, 
+    "quotes": "God can say a thing, but you are the one to create the possibility of that thing.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 18, 
+    "quotes": "Your heart is very pivotal. When your heart starts changing, everything about you changes.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 19, 
+    "quotes": "If you take away instructions, it will lead to destruction.",
+    "category": "Life"
+  },
+  {
+    "id": 20, 
+    "quotes": "You are not successful until you can replicate it in others. True prosperity spreads.",
+    "category": "Life"
+  },
+  {
+    "id": 21, 
+    "quotes": "Life flows in patterns, and every pattern is sustained by principles.",
+    "category": "Life"
+  },
+  {
+    "id": 22, 
+    "quotes": "Understanding vision helps you with self-leadership.",
+    "category": "Life"
+  },
+  {
+    "id": 23, 
+    "quotes": "Following through your process is not about you, but about who you will become.",
+    "category": "Life"
+  },
+  {
+    "id": 24, 
+    "quotes": "Everybody can do business, but not everyone can build a business.",
+    "category": "Life"
+  },
+  {
+    "id": 25, 
+    "quotes": "What you constantly take for granted will cause you to lose opportunities.",
+    "category": "Life"
+  },
+  {
+    "id": 26, 
+    "quotes": "Your mind is like the soil; whatever you sow into it is what you will get out of it.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 27, 
+    "quotes": "Power is the dynamic ability to cause changes.",
+    "category": "Life"
+  },
+  {
+    "id": 28, 
+    "quotes": "The unseen world plays a role in our mindset.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 29, 
+    "quotes": "Evil is a product of famine. When there is famine, evil multiplies.",
+    "category": "Life"
+  },
+  {
+    "id": 30, 
+    "quotes": "The life of joy and peace is the beauty of an abundant life.",
+    "category": "Life"
+  },
+  {
+    "id": 31, 
+    "quotes": "What you see determines what you have. If you cannot see it, you cannot get it.",
+    "category": "Life"
+  },
+  {
+    "id": 32, 
+    "quotes": "Consistency conveys your worth. It shows your commitment and builds people's faith in you.",
+    "category": "Life"
+  },
+  {
+    "id": 33, 
+    "quotes": "Sometimes speaking is healing, and silence is death.",
+    "category": "Life"
+  },
+  {
+    "id": 34, 
+    "quotes": "In the journey of life, you need the escorts of wisdom and favour.",
+    "category": "Life"
+  },
+  {
+    "id": 35, 
+    "quotes": "We give because we are blessed.",
+    "category": "Life"
+  },
+  {
+    "id": 36, 
+    "quotes": "As you rise, have a track record of your rising. It makes kings look for you.",
+    "category": "Life"
+  },
+  {
+    "id": 37, 
+    "quotes": "Do not mistake a canopy for a building.",
+    "category": "Relationships and Marriage"
+  },
+  {
+    "id": 38, 
+    "quotes": "The cheapest way to self-discovery is service.",
+    "category": "Life"
+  },
+  {
+    "id": 39, 
+    "quotes": "A man who ends up becoming self-made will be self-destructive.",
+    "category": "Life"
+  },
+  {
+    "id": 40, 
+    "quotes": "When a man does not have a figure he respects, he will be disfigured in life.",
+    "category": "Life"
+  },
+  {
+    "id": 41, 
+    "quotes": "Your choice determines your path. One right choice can set you on the right path.",
+    "category": "Life"
+  },
+  {
+    "id": 42, 
+    "quotes": "Men show their works; women display their worth.",
+    "category": "Life"
+  },
+  {
+    "id": 43, 
+    "quotes": "The toughness of a man is in his mental abilities.",
+    "category": "Life"
+  },
+  {
+    "id": 44, 
+    "quotes": "Men show their work; women show their worth.",
+    "category": "Life"
+  },
+  {
+    "id": 45, 
+    "quotes": "Mercy changes people, because it changes their hearts first.",
+    "category": "Life"
+  },
+  {
+    "id": 46, 
+    "quotes": "The reflection of excellence is in your pursuit of knowledge.",
+    "category": "Supernatural"
+  },
+  {
+    "id": 47, 
+    "quotes": "Whatever eats into your time, eats into your life.",
+    "category": "Life"
+  },
+  {
+    "id": 48, 
+    "quotes": "Excellent people are thinkers and negotiators.",
+    "category": "Life"
+  },
+  {
+    "id": 49, 
+    "quotes": "Attraction is interested, reality is committed.",
+    "category": "Relationships and Marriage"
+  },
+  {
+    "id": 50, 
+    "quotes": "Your word increases your consciousness, and your consciousness powers your reality.",
+    "category": "Life"
+  },
+  {
+    "id": 51, 
+    "quotes": "Being real with yourself is where reality starts.",
+    "category": "Life"
+  },
+  {
+    "id": 52, 
+    "quotes": "Your attraction can be your greatest temptation.",
+    "category": "Relationships and Marriage"
+  },
+  {
+    "id": 53, 
+    "quotes": "Reality is distinctive, but attraction is manipulative.",
+    "category": "Relationships and Marriage"
+  },
+  {
+    "id": 54, 
+    "quotes": "As a woman, your first duty to a man is to fuel his courage.",
+    "category": "Relationships and Marriage"
+  },
+  {
+    "id": 55, 
+    "quotes": "Attraction is hasty, but reality is patient.",
+    "category": "Relationships and Marriage"
+  },
+  {
+    "id": 56, 
+    "quotes": "Inherent in every woman is a desire to give love and to be loved.",
+    "category": "Relationships and Marriage"
+  },
+  {
+    "id": 57, 
+    "quotes": "Rejection is a chip in your mind that speaks to you negatively.",
+    "category": "Life"
+  }
 ]
-
-
-
 
 export default quotes
